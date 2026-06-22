f546644987880aed5467e5f9fc0086ee94e9c4ee
Aqdas Asghar <aqadsdaqads@gmail.com>
2026-06-22 01:12:56 -0700

    updated-navbar

--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,6 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-[#0d1117]">
       <Navbar />
-      aqdas
       <HeroSlider />
       <ProblemSolutionSection />
       <HowItWorks />
